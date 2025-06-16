4f3140ffd2c040a8d51046f0386e585be7691d32
Parzival <emanuelpedro267@gmail.com>
2025-06-15 22:20:49 -0300

    atualizacao30

--- config/db.js
+++ config/db.js
@@ -1,16 +1,24 @@
 const mysql = require('mysql2/promise');
 
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '083665',
-  database: 'sistema_studioarte',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+// const db = mysql.createPool({
+//   host: 'mysql.hostinger.com',
+//   user: 'u491967152_jessica01',
+//   password: 'Ca083665@',
+//   database: 'u491967152_sistema_studio',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+ const db = mysql.createPool({
+   host: 'localhost',
+   user: 'root',
+   password: '083665',
+   database: 'sistema_studioarte',
+   waitForConnections: true,
+   connectionLimit: 10,
+   queueLimit: 0
 });
-
-
 
 
 module.exports = db;
