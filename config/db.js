aa568b562bef63e29f36cbc9abe87fbda01f4932
Parzival <emanuelpedro267@gmail.com>
2025-06-22 14:55:29 -0300

    atualizacaoNova

--- config/db.js
+++ config/db.js
@@ -10,13 +10,13 @@ const mysql = require('mysql2/promise');
 // queueLimit: 0
 
 
-const db = mysql.createPool({
-st: '127.0.0.1',
-er: 'studiouser',
-ssword: '083665',
-tabase: 'sistema_studioarte',
-itForConnections: true,
-nnectionLimit: 10,
+ const db = mysql.createPool({
+ host: '127.0.0.1',
+ user: 'studiouser',
+ password: '083665',
+ database: 'sistema_studioarte',
+ waitForConnections: true,
+ connectionLimit: 10,
  queueLimit: 0
 
 });
