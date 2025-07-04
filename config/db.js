93c6f710b2dab8528348ab75da85e46118886e2c
Parzival <emanuelpedro267@gmail.com>
2025-07-04 07:58:28 -0300

    correcaoBd

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
  //const mysql = require('mysql2promise');
-  const mysql = require('mysql2promise');
+  const mysql = require('mysql2/promise');
     //  const db = mysql.createPool({
     //  host: 'localhost',
     //  user: 'root',
